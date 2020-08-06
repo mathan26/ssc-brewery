b2525f8ffde3b51028c32266d338a9311db9113a
mathan26 <mathan.se@gmail.com>
2020-08-06 18:02:31 +0530

    ITtest :white_check_mark: Permit the Restcontroller get
    :boom: -
    
    :pushpin: antmatchers we can use HTTPMethod.GET, "URL"

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package guru.sfg.brewery.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,8 +17,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     authorize.
                             antMatchers("/", "/webjars/**", "/login", "/resources/**")
                             .permitAll()
-                            .antMatchers("/beers/find", "/beers*")
-                            .permitAll();
+                            .antMatchers("/beers/find", "/beers*").permitAll()
+                            .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll();
+
 
                 } )
                 .authorizeRequests()
