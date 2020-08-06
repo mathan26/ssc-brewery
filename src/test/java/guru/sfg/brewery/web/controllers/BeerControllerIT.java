c90756d8499084a3c4b08e2befae35bf5110e947
mathan26 <mathan.se@gmail.com>
2020-08-06 17:24:06 +0530

    ITtest :white_check_mark: Permit the beer/find
    :boom: -
    
    :pushpin: We can use multiple antmatchers after permitAll
              Test the view name and model attribute

---
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -0,0 +1,22 @@
+package guru.sfg.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest
+class BeerControllerIT extends BaseIT{
+
+    @Test
+    public void findBeer()throws Exception{
+        mockMvc.perform(get("/beers/find").with(anonymous()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));;
+    }
+}
