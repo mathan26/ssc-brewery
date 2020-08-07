60de74ced03748b22b1a4ecf263cee360ba01ff6
mathan26 <mathan.se@gmail.com>
2020-08-07 19:14:33 +0530

    Add new user :sparkles: CSRF issue fixed by disabling :boom: - :pushpin:

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -34,7 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.addFilterBefore(restHeaderAuthFilter(authenticationManager()),
-                UsernamePasswordAuthenticationFilter.class);
+                UsernamePasswordAuthenticationFilter.class)
+                .csrf().disable();
 
         http.authorizeRequests(authorize -> {
                     authorize
