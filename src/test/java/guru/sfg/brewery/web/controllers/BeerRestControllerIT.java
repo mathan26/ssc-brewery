83f85086f0dfedd17fb1994de23222dacf068fc9
mathan26 <mathan.se@gmail.com>
2020-08-07 19:42:05 +0530

    Add new user :sparkles: unsuccessfulAuthentication overridden to catch exception :boom: - :pushpin:

--- src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
@@ -47,4 +47,10 @@ public class BeerRestControllerIT extends BaseIT {
         mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311"))
                 .andExpect(status().isUnauthorized());
     }
+    @Test
+    void deleteBeerBadCreds() throws Exception{
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                .header("Api-Key","spring").header("Api-Secret", "guruXXXX"))
+                .andExpect(status().isUnauthorized());
+    }
 }
