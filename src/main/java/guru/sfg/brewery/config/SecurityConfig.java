05b51278b2165704449a00d055c0719831c30402
mathan26 <mathan.se@gmail.com>
2020-08-09 15:42:15 +0530

    :sparkles: updating get beer security config :boom: - :pushpin: updated the Junits and  the role access added

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -21,17 +21,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests(authorize -> {
+        http
+                .authorizeRequests(authorize -> {
                     authorize
-                            .antMatchers("/h2-console/**").permitAll() //should not use in production
+                            .antMatchers("/h2-console/**").permitAll() //do not use in production!
                             .antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
-                            .antMatchers("/beers/find", "/beers*").permitAll()
-                            .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
-                            .mvcMatchers(HttpMethod.DELETE,"/api/v1/beer/**").hasRole("ADMIN")
-                            .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll()
-                            .mvcMatchers("/brewery/breweries").hasAnyRole("ADMIN","CUSTOMER")
-                            .mvcMatchers(HttpMethod.GET, "/brewery/api/v1/breweries").hasAnyRole("ADMIN","CUSTOMER");
-                 })
+                            .antMatchers(HttpMethod.GET, "/api/v1/beer/**")
+                            .hasAnyRole("ADMIN", "CUSTOMER", "USER")
+                            .mvcMatchers(HttpMethod.DELETE, "/api/v1/beer/**").hasRole("ADMIN")
+                            .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}")
+                            .hasAnyRole("ADMIN", "CUSTOMER", "USER")
+                            .mvcMatchers("/brewery/breweries")
+                            .hasAnyRole("ADMIN", "CUSTOMER")
+                            .mvcMatchers(HttpMethod.GET, "/brewery/api/v1/breweries")
+                            .hasAnyRole("ADMIN", "CUSTOMER")
+                            .mvcMatchers("/beers/find", "/beers/{beerId}")
+                            .hasAnyRole("ADMIN", "CUSTOMER", "USER");
+                } )
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
