0bc90e8b276bf762323d960555292306103a4681
mathan26 <mathan.se@gmail.com>
2020-08-11 14:26:03 +0530

    :sparkles: adding tests, securing read order :boom: - :pushpin:

--- src/main/java/guru/sfg/brewery/web/controllers/api/BeerOrderController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/api/BeerOrderController.java
@@ -5,6 +5,7 @@ import guru.sfg.brewery.web.model.BeerOrderDto;
 import guru.sfg.brewery.web.model.BeerOrderPagedList;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -25,6 +26,9 @@ public class BeerOrderController {
         this.beerOrderService = beerOrderService;
     }
 
+    @PreAuthorize("hasAuthority('order.read') OR " +
+            "hasAuthority('customer.order.read') " +
+            " AND @beerOrderAuthenticationManger.customerIdMatches(authentication, #customerId )")
     @GetMapping("orders")
     public BeerOrderPagedList listOrders(@PathVariable("customerId") UUID customerId,
                                          @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
@@ -41,12 +45,16 @@ public class BeerOrderController {
         return beerOrderService.listOrders(customerId, PageRequest.of(pageNumber, pageSize));
     }
 
+
     @PostMapping("orders")
     @ResponseStatus(HttpStatus.CREATED)
     public BeerOrderDto placeOrder(@PathVariable("customerId") UUID customerId, @RequestBody BeerOrderDto beerOrderDto){
         return beerOrderService.placeOrder(customerId, beerOrderDto);
     }
 
+    @PreAuthorize("hasAuthority('order.read') OR " +
+            "hasAuthority('customer.order.read') " +
+            " AND @beerOrderAuthenticationManger.customerIdMatches(authentication, #customerId )")
     @GetMapping("orders/{orderId}")
     public BeerOrderDto getOrder(@PathVariable("customerId") UUID customerId, @PathVariable("orderId") UUID orderId){
         return beerOrderService.getOrderById(customerId, orderId);
