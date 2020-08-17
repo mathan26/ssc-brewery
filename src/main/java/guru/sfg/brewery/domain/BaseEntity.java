2cc41370e7aecda2f353bebed499065b1d939006
mathan26 <mathan.se@gmail.com>
2020-08-17 17:03:01 +0530

    :sparkles: Enabling csrf :boom: - :pushpin:

--- src/main/java/guru/sfg/brewery/domain/BaseEntity.java
+++ src/main/java/guru/sfg/brewery/domain/BaseEntity.java
@@ -37,14 +37,6 @@ import java.util.UUID;
 @NoArgsConstructor
 @MappedSuperclass
 public class BaseEntity {
-
-    public BaseEntity(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate) {
-        this.id = id;
-        this.version = version;
-        this.createdDate = createdDate;
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(
@@ -68,4 +60,13 @@ public class BaseEntity {
     public boolean isNew() {
         return this.id == null;
     }
+
+    public BaseEntity(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate) {
+        this.id = id;
+        this.version = version;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+
 }
