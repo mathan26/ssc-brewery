1ed8b072f790a9706ca5474a9a027c1fe16f9e66
mathan26 <mathan.se@gmail.com>
2020-08-19 15:31:47 +0530

    :sparkles: adding lock user account :boom: - :pushpin: get the user failed login count if it is more than 3 lock the account

--- src/main/java/guru/sfg/brewery/domain/security/LoginFailureRepository.java
+++ src/main/java/guru/sfg/brewery/domain/security/LoginFailureRepository.java
@@ -1,8 +1,11 @@
 package guru.sfg.brewery.domain.security;
 
-import guru.sfg.brewery.domain.security.LoginFailure;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 
 public interface LoginFailureRepository extends JpaRepository<LoginFailure, Integer> {
+    List<LoginFailure> findAllByUserAndCreatedDateIsAfter(User user, Timestamp timestamp);
 }
